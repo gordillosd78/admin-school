7c36f31f5818f7b2be175d14c61f912e8794b206
Sergio Gordillo <gordillo78@gmail.com>
2023-05-04 09:35:13 -0300

    more configurations

--- backend/assets/AppAsset.php
+++ backend/assets/AppAsset.php
@@ -19,6 +19,5 @@ class AppAsset extends AssetBundle
         'yii\web\YiiAsset',
         'yii\bootstrap5\BootstrapAsset',
         '\rmrevin\yii\fontawesome\AssetBundle'
-
     ];
 }
--- backend/controllers/SiteController.php
+++ backend/controllers/SiteController.php
@@ -3,6 +3,7 @@
 namespace backend\controllers;
 
 use common\models\LoginForm;
+use rmrevin\yii\fontawesome\FontAwesome;
 use Yii;
 use yii\filters\VerbFilter;
 use yii\filters\AccessControl;
@@ -78,16 +79,63 @@ class SiteController extends Controller
 
         $this->layout = 'blank';
 
+        $items = array();
         $model = new LoginForm();
+
         if ($model->load(Yii::$app->request->post()) && $model->login()) {
-            return $this->goBack();
+
+            if (!Yii::$app->user->can('admin')) {
+                Yii::$app->user->logout();
+                return $this->goHome();
+            } else {
+                $auth = Yii::$app->authManager;
+                $session = Yii::$app->session;
+                $roles = $auth->getAssignments(Yii::$app->user->id);
+                foreach ($roles as $value) {
+                    $child = $auth->getChildren($value->roleName);
+                    foreach ($child as $child_item) {
+                        if (strpos($child_item->description, ","))
+                            list($icono, $descripcion) = explode(",", $child_item->description);
+
+                        //if (in_array($child_item->name, $model->sistema)) // -- Menu sistema
+                        //  $DropDownSistemaItems[] = ['label' => FontAwesome::icon($icono) . ' ' . $descripcion, 'url' => [$child_item->name . "/index"]];
+                        if (in_array($child_item->name, $model->procesos)) // -- Menu procesos
+                            $DropDownProcesosItems[] = ['label' => FontAwesome::icon($icono) . ' ' . $descripcion, 'url' => [$child_item->name . "/index"]];
+                        // elseif (in_array($child_item->name, $model->admin)) // -- Menu Administrar
+                        //   $DropDownAdminItems[] = ['label' => FontAwesome::icon($icono) . ' ' . $descripcion, 'url' => [$child_item->name . "/index"]];
+                        // elseif (in_array($child_item->name, $model->tipos)) // -- Menu Tipos
+                        //   $DropDownTiposAdmin[] = ['label' => FontAwesome::icon($icono) . ' ' . $descripcion, 'url' => [$child_item->name . "/index"]];
+                        // elseif (in_array($child_item->name, $model->articulos)) // -- Menu Articulos
+                        //   $DropDownArticulos[] = ['label' => FontAwesome::icon($icono) . ' ' . $descripcion, 'url' => [$child_item->name . "/index"]];
+                        else
+                            $items[] = ['label' => FontAwesome::icon($icono) . ' ' . $descripcion, 'url' => [$child_item->name . "/index"]];
+                    }
+                }
+                if (isset($DropDownProcesosItems))
+                    $items[] = $this->setMenuByArray($DropDownProcesosItems, 'database', 'Procesos');
+                //if (isset($DropDownArticulos))
+                //  $DropDownAdminItems[] = $this->setMenuByArray($DropDownArticulos, 'boxes', 'Articulos');
+                //if (isset($DropDownTiposAdmin))
+                //  $DropDownAdminItems[] = $this->setMenuByArray($DropDownTiposAdmin, 'database', 'Tipos');
+                // if (isset($DropDownAdminItems))
+                //   $items[] = $this->setMenuByArray($DropDownAdminItems, 'cogs', 'Administrar');
+                //if (isset($DropDownSistemaItems))
+                //  $items[] = $this->setMenuByArray($DropDownSistemaItems, 'bars', 'Sistema');
+
+                if ($session->isActive) {
+                    $session->set('items', $items);
+                }
+
+
+                return $this->goBack();
+            }
+        } else {
+            $model->password = '';
+
+            return $this->render('login', [
+                'model' => $model,
+            ]);
         }
-
-        $model->password = '';
-
-        return $this->render('login', [
-            'model' => $model,
-        ]);
     }
 
     /**
@@ -100,5 +148,21 @@ class SiteController extends Controller
         Yii::$app->user->logout();
 
         return $this->goHome();
+    }
+
+    /**
+     * Establece formato al array usado para los menus
+     * 
+     * @param array $menu array con valores previo ya cargados
+     * @param string $icon icono que se utilizara
+     * @param string $name nombre que se muestra en el menu
+     * @return array
+     */
+    public function setMenuByArray($array, $icon, $name)
+    {
+        return [
+            'label' => FontAwesome::icon($icon) . ' ' . $name,
+            'items' => $array
+        ];
     }
 }
